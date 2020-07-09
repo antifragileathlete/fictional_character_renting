3492b724042181247a3aab13387d650d5530b330
Adrian Schultze <adrianschultze@gmail.com>
2020-07-09 20:39:55 +0200

    added show index and create view, added some routes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,12 +4,28 @@ class CharactersController < ApplicationController
   end
 
   def show
+    @character = Character.find(params[:id])
   end
 
   def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
 
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :genre, :user_id)
+  end
 end
