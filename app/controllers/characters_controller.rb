73a863ce1267e2efd2b4852fef307551a0c25f3d
Adrian Schultze <adrianschultze@gmail.com>
2020-07-09 21:11:50 +0200

    added new and index and show, nex and index works for character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,18 +1,23 @@
 class CharactersController < ApplicationController
   def index
-    @characters = Character.all
+    #@characters = Character.all
+    @characters = policy_scope(Character)
+    #authorize @characters
   end
 
   def show
     @character = Character.find(params[:id])
+    authorize @character
   end
 
   def new
     @character = Character.new
+    authorize @character
   end
 
   def create
     @character = Character.new(character_params)
+    authorize @character
     if @character.save
       redirect_to character_path(@character)
     else
@@ -21,6 +26,7 @@ class CharactersController < ApplicationController
   end
 
   def destroy
+    # authorize @character
   end
 
   private
