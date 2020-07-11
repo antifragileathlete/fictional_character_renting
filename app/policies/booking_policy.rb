4443fbf27a0d1003cd4ff2e6a611ae9dcdd7e3d1
Maria Fernandez <mariapop81@gmail.com>
2020-07-11 14:59:06 +0200

    booking policy

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,24 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def create?
+      return true
+    end
+
+    def new?
+      return true
+    end
+
+    def update?
+      record.user == user
+    end
+
+    def destroy?
+      record.user == user
+    end
+
+  end
+end
