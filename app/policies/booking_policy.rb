b8e19e172f9644eba687d06f8939e446c77c0372
Adrian Schultze <adrianschultze@gmail.com>
2020-07-11 16:37:46 +0200

    pundit stuff again

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,24 +1,31 @@
 class BookingPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
+  # class Scope < Scope
+  def resolve
+    scope.all
+  end
 
-    def create?
-      return true
-    end
+  def create?
+    return true
+  end
 
-    def new?
-      return true
-    end
+  def new?
+    return true
+  end
 
-    def update?
-      record.user == user
-    end
+  def update?
+    record.user == user
+  end
 
-    def destroy?
-      record.user == user
-    end
+  def destroy?
+    record.user == user
+  end
 
+  def booking?
+    true
+  end
+
+  def booking
+    true
   end
 end
+# end
