3f283c533cdf747d5ff7746b29a3f77a7204b2aa
Johanna Petrich <pejotrich@gmail.com>
2020-07-11 12:17:17 +0200

    fixed required user id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+file = URI.open("https://res.cloudinary.com/dnfzsh177/image/upload/v1594289736/Characters/Characters/Green_Arrow.jpg")
+character = Character.new(name: "Green Arrow", genre: "Superhero", user_id: 1)
+character.photo.attach(io: file, filename: "character.png", content_type: 'image/png')
+character.save!
