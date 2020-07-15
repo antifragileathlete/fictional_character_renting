e0bb56e8b90ced9ed9d37d7a151e747c737e8738
Johanna Petrich <pejotrich@gmail.com>
2020-07-15 15:46:48 +0200

    made datepickr work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-install flatpickr from 'flatpickr';
+import flatpickr from 'flatpickr'
+import "flatpickr/dist/flatpickr.min.css"
+
+
+/* document.addEventListener("turbolinks:load", () => {
+  flatpickr("[data-behaviour='flatpickr']", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+  })
+}) */
+
+flatpickr(".flatpickr"), {
+
+}
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
