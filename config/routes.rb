01b0c1b1a16447be12f1610e6e52ceb7fff733c2
Johanna Petrich <pejotrich@gmail.com>
2020-07-17 10:47:59 +0200

    made basic accept/decline feature work

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :characters, only: %i[index show new create destroy] do
-    resources :bookings, only: [ :new, :create, :show ]
+    resources :bookings, only: [ :new, :create, :show ] do 
+      post :accept
+      post :decline
+    end
   end
-  resources :bookings, only: [ :destroy, :index]
+  resources :bookings, only: [ :destroy, :index] do
+    
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_120055) do
+ActiveRecord::Schema.define(version: 2020_07_16_185440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_120055) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "end_date"
     t.bigint "user_id", null: false
+    t.string "status"
     t.index ["character_id"], name: "index_bookings_on_character_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
